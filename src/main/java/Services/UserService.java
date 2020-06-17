991e6458eef5a8f95fcc6fa1c4f4de15360d0732
szmecskas <62383318+szmecskas@users.noreply.github.com>
2020-06-17 21:28:32 +0300

    Update UserService.java

--- src/main/java/Services/UserService.java
+++ src/main/java/Services/UserService.java
@@ -1,17 +1,17 @@
 package Services;
 
+import Controllers.ListTrainersController;
 import Exceptions.CouldNotWriteUsersException;
 import Exceptions.EmptyFieldException;
 import Exceptions.IncorrectLoginData;
 import Model.*;
+import Model.Participant;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.commons.io.FileUtils;
 
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
+import java.lang.reflect.Array;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -29,63 +29,68 @@ import java.util.Objects;
 public class UserService {
 
     /*List of participants*/
-    private static List<Participant> participants  = new ArrayList<Participant>();
+    private static List<Participant> participants;
+
+    static {
+        participants=new ArrayList<Participant>();
+    }
 
     /*List of trainers*/
-    private static List<Trainer> trainers = new ArrayList<Trainer>();
+    private static List<Trainer> trainers;
 
-    /*List of sports*/
-    private static List<Sport> sports = new ArrayList<Sport>();
+    static {
+        trainers=new ArrayList<Trainer>();
+    }
 
     /*Path for participants: participants_PATH*/
-    //private static final Path participants_PATH = FileSystemService.getPathToFile("config", "participants.json");
     private static final Path participants_PATH = FileSystemService.getPathToFile("config", "participants.json");
-
+    
     /*Path for trainers: trainers_PATH*/
     private static final Path trainers_PATH = FileSystemService.getPathToFile("config", "trainers.json");
+    int sw;
+    private static ListTrainersController ltc;
 
     
     /*********************************************************************************************
     * 
     * 
     * This method loads the users: both participants and trainers, from the corresponding files.
-    *
-    *
+    * 
+     * 
+     * 
     * *********************************************************************************************/
     public static void loadUsersFromFile() throws IOException {
 
-        System.out.println("intra in functia de load users from file");
         if (!Files.exists(participants_PATH)) {
-            FileUtils.copyURLToFile((Objects.requireNonNull(UserService.class.getClassLoader().getResource("participants.json"))), participants_PATH.toFile());
+            FileUtils.copyURLToFile(Objects.requireNonNull(UserService.class.getClassLoader().getResource("participants.json")), participants_PATH.toFile());
         }
-        System.out.println("face object mapper de participanti");
-        ObjectMapper objectMapperParticipants = new ObjectMapper();
-        System.out.println("incepe sa citeasca participanti");
 
-        participants = objectMapperParticipants.readValue(participants_PATH.toFile(), new TypeReference<List<Participant>>() {});
-        System.out.println("a citit participanti");
+        ObjectMapper objectMapperparticipants = new ObjectMapper();
+
+        participants = objectMapperparticipants.readValue(participants_PATH.toFile(), new TypeReference<List<Participant>>() {
+        });
 
         if (!Files.exists(trainers_PATH)) {
-            FileUtils.copyURLToFile((Objects.requireNonNull(UserService.class.getClassLoader().getResource("trainers.json"))), trainers_PATH.toFile());
+            FileUtils.copyURLToFile(Objects.requireNonNull(UserService.class.getClassLoader().getResource("trainers.json")), trainers_PATH.toFile());
         }
-        System.out.println("face object mapper de traineri");
-        ObjectMapper objectMapperTrainers = new ObjectMapper();
-        System.out.println("citeste traineri");
-        trainers = objectMapperTrainers.readValue(trainers_PATH.toFile(), new TypeReference<List<Trainer>>() {
-        });
-        System.out.println("a citit traineri");
-    }
 
+        ObjectMapper objectMappertrainers = new ObjectMapper();
+
+        trainers = objectMappertrainers.readValue(trainers_PATH.toFile(), new TypeReference<List<Trainer>>() {
+        });
+    } 
+
+    
     /* *********************************************
     *
     *   This method adds the participant.
     *
     * **********************************************/
-    public static void addParticipant(String firstName, String lastName, String address, String username, String password, String university, String specialization, String uniqueID, String studyYear, boolean healthAppropval, List<Application> applications, Contract contract)throws Exception {
+    public static void addParticipant(String firstName, String lastName, String username, String password, String university, String specialization, String uniqueID, String studyYear , boolean healthAppropval)throws Exception {
 
         checkEmptyField(username,password);
-        //checkUsernameAlreadyExist(username);
-        participants.add(new Participant(firstName, lastName,  address,  username,  encodePassword(username, password),  university,  specialization,  uniqueID,  studyYear,  healthAppropval, null,null));
+        checkUsernameAlreadyExist(username);
+        participants.add(new Participant(firstName, lastName,  username,  encodePassword(username, password),  university,  specialization,  uniqueID,  studyYear,  healthAppropval,null));
         persistParticipants();
     }
     
@@ -95,11 +100,11 @@ public class UserService {
      *   This method adds the trainer.
      *
      * **********************************************/
-    public static void addTrainer(String firstName, String lastName, String address, String username, String password, String university, List<Contract> contracts, List<Sport> sports)throws Exception {
+    public static void addTrainer(java.lang.String firstName, java.lang.String lastName, java.lang.String username, java.lang.String password, java.lang.String university, List<Application> application, java.lang.String sports, List<Date> dates)throws Exception {
 
-        //checkEmptyField(username,password);
-        //checkUsernameAlreadyExist(username);
-        trainers.add(new Trainer(firstName, lastName,  username,   encodePassword(username, password),  address,  university, null,null));
+        checkEmptyField(username,password);
+        checkUsernameAlreadyExist(username);
+        trainers.add(new Trainer(firstName, lastName,  username,   encodePassword(username, password),  university, application, sports, dates));
         persistTrainers();
     }
  
@@ -113,13 +118,10 @@ public class UserService {
     private static void persistParticipants() {
 
         try {
-            System.out.println("? persistParticipants");
 
             ObjectMapper objectMapper = new ObjectMapper();
-
             objectMapper.writerWithDefaultPrettyPrinter().writeValue(participants_PATH.toFile(), participants);
 
-            System.out.println("Scrie in fisier");
         } catch (IOException e) {
             throw new CouldNotWriteUsersException();
         }
@@ -138,7 +140,6 @@ public class UserService {
             ObjectMapper objectMapper = new ObjectMapper();
             objectMapper.writerWithDefaultPrettyPrinter().writeValue(trainers_PATH.toFile(), trainers);
 
-            System.out.println("Scrie in fisierul de traineri ");
         } catch (IOException e) {
             throw new CouldNotWriteUsersException();
         }
@@ -152,15 +153,15 @@ public class UserService {
     *
     *
     * *****************************************************/
-    private static void checkUsernameAlreadyExist(String username) throws Exception {
-        System.out.println(" ///////// ? checkUsernameAlreadyExist");
+    private static void checkUsernameAlreadyExist(java.lang.String username) throws Exception {
+
         /*Participants' usernames are checked.*/
-        for (Participant participant : participants) {
-            System.out.println(" ///////// ? ////// for checkUsernameAlreadyExist");
+     /*   for (Participant participant : participants) {
+            System.out.println("Faci?");
             if (Objects.equals(username, participant.getUsername()))
                 throw new CouldNotWriteUsersException();//change exception
-        }
-        System.out.println("?checkUsernameAlreadyExist -> trainers");
+        }*/
+        System.out.println("Faci2?");
         /*Trainers' usernames are checked.*/
         for (Trainer trainer : trainers) {
             if (Objects.equals(username, trainer.getUsername()))
@@ -174,7 +175,8 @@ public class UserService {
     *  This method checks if an attribute is empty.
     *
     * **********************************************/
-    private static void checkEmptyField(String username, String password) throws EmptyFieldException {
+    private static void checkEmptyField(java.lang.String username, java.lang.String password) throws EmptyFieldException {
+
         if(username.equals("") || password.equals("")) throw new EmptyFieldException();
     }
 
@@ -184,41 +186,34 @@ public class UserService {
      *  This method checks if the login credentials are correct.
      *
      * ********************************************************/
-    public static void checkLoginCredentials(String username,String password) throws IncorrectLoginData {
+    public static void checkLoginCredentials(java.lang.String username, java.lang.String password, java.lang.String role) throws IncorrectLoginData {
+        int sw=0;
+        if(role.equals("Trainer")) {
+            for (Trainer trainer : trainers) {
+                if (Objects.equals(username, trainer.getUsername())) {
+                    sw=1;
+                    if (!Objects.equals(password, trainer.getPassword()))
+                        throw new IncorrectLoginData();
+                }
+            }
+            if(sw==0) //n-am gassit user ul
+                throw new IncorrectLoginData();
+        }
+        else
+        if(role.equals("Participant")) {
+            for (Participant participant : participants) {
+                if (Objects.equals(username, participant.getUsername())) {
+                    if(Objects.equals(password, participant.getPassword()))
+                        ListTrainersController.setActiveParticipant(participant);
+                    sw=1;
+                    if (!Objects.equals(password, participant.getPassword()))
+                        throw new IncorrectLoginData();
+                }
+            }
+            if(sw==0) //n-am gassit user ul
+                throw new IncorrectLoginData();
+        }
 
-//      try {
-//          loadUsersFromFile();
-//      } catch (IOException e) {
-//          e.printStackTrace();
-//      }
-
-//      System.out.println("aaaaaaaaaa 1");
-//        String encodePassword = encodePassword(username, password);
-//        System.out.println("aaaaaaaaaa 2");
-//        /*The participants' usernames and passwords are checked*/
-//        int correct=0;
-//        System.out.println("aaaaaaaaaa 3");
-//        for (Participant participant : participants) {
-//            System.out.println("aaaaaaaaaa 4");
-//            if (Objects.equals(username, participant.getUsername())) {
-//                correct=1;
-//                System.out.println("aaaaaaaaaa 5");
-//                if (!Objects.equals(encodePassword, participant.getPassword()))
-//                    throw new IncorrectLoginData();
-//            }
-//        }
-//        System.out.println("aaaaaaaaaa 6");
-//        /*The trainers' usernames and passwords are checked*/
-//        for (Trainer trainer : trainers) {
-//            if (Objects.equals(username, trainer.getUsername())) {
-//                correct=1;
-//                if (!Objects.equals(encodePassword, trainer.getPassword()))
-//                    throw new IncorrectLoginData();
-//            }
-//        }
-//
-//        /*The inexistance of the username and password leads to an exception.*/
-//        if(correct==0) throw new IncorrectLoginData();
     }
 
 
@@ -227,7 +222,7 @@ public class UserService {
      *  This method encodes the password
      *
      * ********************************************************/
-    private static String encodePassword(String salt, String password) {
+    private static java.lang.String encodePassword(java.lang.String salt, java.lang.String password) {
 
         MessageDigest md = getMessageDigest();
         md.update(salt.getBytes(StandardCharsets.UTF_8));
@@ -235,7 +230,7 @@ public class UserService {
         byte[] hashedPassword = md.digest(password.getBytes(StandardCharsets.UTF_8));
 
         /*This is the way a password should be encoded when checking the credentials*/
-        return new String(hashedPassword, StandardCharsets.UTF_8)
+        return new java.lang.String(hashedPassword, StandardCharsets.UTF_8)
                 .replace("\"", ""); /*to be able to save in JSON format*/
     }
 
@@ -253,4 +248,49 @@ public class UserService {
         return md;
     }
 
+    public static void setTrainers(java.lang.String sport) {
+            for (Trainer trainer : trainers) {
+                System.out.println(sport+" "+trainer.getSports().toString());
+                if(trainer.getSports().equals(sport)) {
+                    System.out.println("lsa vedem");
+                    ltc.getTrainer().getItems().add(trainer.getFirstName());
+                }
+            }
+        }
+    public static void setDate(java.lang.String name_trainer) {
+        for (Trainer trainer : trainers) {
+            if(name_trainer.equals(trainer.getFirstName())) {
+                java.lang.String date="";
+                for(Date date1:trainer.getDates() ) {
+                    date = date1.getDay() + "-" + date1.getStartHour() + ":" + date1.getStartMinute() + "-" + date1.getEndHour() + ":" + date1.getEndMinute();
+                    ltc.getDate().getItems().add(date);
+                    date = "";
+                }
+            }
+        }
+    }
+
+    public static Trainer setByTrainer(java.lang.String firstName) {
+        for(Trainer trainer:trainers) {
+            if(trainer.getFirstName().equals(firstName))
+                return trainer;
+        }
+        return null;
+    }
+
+    public static void injecltc(ListTrainersController u) {
+        ltc= u;
+    }
+
+
+    public static void sentParticiapnt(String participant, Application application) {
+        for(Participant participant1:participants) {
+            String name=participant1.getFirstName()+" "+participant1.getLastName();
+            if(name.equals(participant)) {
+                System.out.println("intra in if");
+                participant1.getApplications().add(application);
+            }
+        }
+        persistParticipants();
+    }
 }
